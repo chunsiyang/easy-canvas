2a3f21e0fc3f1092824ef1ce06822c6c3613ed51
chuns <chunsi.yang@gmail.com>
2020-09-30 18:17:01 +1000

    add email error log

--- app/tools/email_tools.py
+++ app/tools/email_tools.py
@@ -4,27 +4,31 @@ from email.header import Header
 from email.mime.text import MIMEText
 
 from app.tools.config_tools import get_config, APP_CONFIG
+from app.tools.log_tools import log
 
 
 def send_email(to, subject, text):
-    config = get_config(APP_CONFIG)['EMAIL']
-    mail_info = {
-        "from": config['EMAIL_FROM'],
-        "to": to,
-        "host": config['EMAIL_HOST'],
-        "username": config['EMAIL_USERNAME'],
-        "password": config['EMAIL_PASSWORD'],
-        "subject": subject,
-        "text": text,
-        "encoding": "utf-8"
-    }
-    smtp = SMTP_SSL(mail_info["host"])
-    smtp.set_debuglevel(1)
-    smtp.ehlo(mail_info["host"])
-    smtp.login(mail_info["username"], mail_info["password"])
-    msg = MIMEText(mail_info["text"], "plain", mail_info["encoding"])
-    msg["Subject"] = Header(mail_info["subject"], mail_info["encoding"])
-    msg["from"] = mail_info["from"]
-    msg["to"] = mail_info["to"]
-    smtp.sendmail(mail_info["from"], mail_info["to"], msg.as_string())
-    smtp.quit()
+    try:
+        config = get_config(APP_CONFIG)['EMAIL']
+        mail_info = {
+            "from": config['EMAIL_FROM'],
+            "to": to,
+            "host": config['EMAIL_HOST'],
+            "username": config['EMAIL_USERNAME'],
+            "password": config['EMAIL_PASSWORD'],
+            "subject": subject,
+            "text": text,
+            "encoding": "utf-8"
+        }
+        smtp = SMTP_SSL(mail_info["host"])
+        smtp.set_debuglevel(1)
+        smtp.ehlo(mail_info["host"])
+        smtp.login(mail_info["username"], mail_info["password"])
+        msg = MIMEText(mail_info["text"], "plain", mail_info["encoding"])
+        msg["Subject"] = Header(mail_info["subject"], mail_info["encoding"])
+        msg["from"] = mail_info["from"]
+        msg["to"] = mail_info["to"]
+        smtp.sendmail(mail_info["from"], mail_info["to"], msg.as_string())
+        smtp.quit()
+    except Exception as e:
+        log('info', 'Email send error: %s' % e)
